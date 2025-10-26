fb1ee47361f45378e884ac35208f5cb974430d8e
Omm Prasad Nath <nathommprasad@gmail.com>
2025-10-26 22:30:44 +0530

    Add files via upload

---
+++ birthday.js
@@ -0,0 +1,356 @@
+
+
+const targetDate = new Date("16 September 2025 11:59:00").getTime();
+const countdownWrapper = document.getElementById("countdown-wrapper");
+
+const countdownInterval = setInterval(() => {
+    const now = new Date().getTime();
+    const distance = targetDate - now;
+
+    const days = Math.floor(distance / (1000 * 60 * 60 * 24));
+    const hours = Math.floor((distance % (1000 * 60 * 60 * 24)) / (1000 * 60 * 60));
+    const minutes = Math.floor((distance % (1000 * 60 * 60)) / (1000 * 60));
+    const seconds = Math.floor((distance % (1000 * 60)) / 1000);
+
+    document.getElementById("days").innerText = days.toString().padStart(2, '0');
+    document.getElementById("hours").innerText = hours.toString().padStart(2, '0');
+    document.getElementById("minutes").innerText = minutes.toString().padStart(2, '0');
+    document.getElementById("seconds").innerText = seconds.toString().padStart(2, '0');
+
+    // When countdown is done
+    if (distance <= 0) {
+        clearInterval(countdownInterval);
+        countdownWrapper.style.display = "none"; 
+
+        // Start music prompt and animation
+        Swal.fire({
+            title: 'Do you want to play music in the background?',
+            icon: 'question',
+            showCancelButton: true,
+            confirmButtonText: 'Yes',
+            cancelButtonText: 'No',
+            confirmButtonColor: '#3085d6',
+            cancelButtonColor: '#d33',
+        }).then((result) => {
+            const music = document.querySelector('.song');
+            if (result.isConfirmed && music) {
+                music.play().catch(err => console.log("Autoplay blocked:", err));
+            }
+            animationTimeline(); 
+        });
+    }
+}, 1000);
+
+
+// animation timeline
+const animationTimeline = () => {
+   
+    const textBoxChars = document.getElementsByClassName("hbd-chatbox")[0];
+    const hbd = document.getElementsByClassName("wish-hbd")[0];
+
+    textBoxChars.innerHTML = `<span>${textBoxChars.innerHTML
+        .split("")
+        .join("</span><span>")}</span>`;
+
+    hbd.innerHTML = `<span>${hbd.innerHTML
+        .split("")
+        .join("</span><span>")}</span>`;
+
+    const ideaTextTrans = {
+        opacity: 0,
+        y: -20,
+        rotationX: 5,
+        skewX: "15deg"
+    }
+
+    const ideaTextTransLeave = {
+        opacity: 0,
+        y: 20,
+        rotationY: 5,
+        skewX: "-15deg"
+    }
+
+    // timeline
+    const tl = new TimelineMax();
+
+    tl.to(".container", 0.6, {
+        visibility: "visible"
+    })
+    .from(".one", 0.7, {
+        opacity: 0,
+        y: 10
+    })
+ 
+
+    .from(".two", 0.4, {
+        opacity: 0,
+        y: 10
+    })
+    .to(".one",
+        0.7,
+        {
+            opacity: 0,
+            y: 10
+        },
+    "+=3.5")
+   
+  
+    .to(".two",
+        0.7,
+        {
+            opacity: 0,
+            y: 10
+        },
+    "-=1")
+    .from(".three", 0.7, {
+        opacity: 0,
+        y: 10
+    })
+    .to(".three",
+        0.7,
+        {
+            opacity: 0,
+            y: 10
+        },
+    "+=3")
+    .from(".four", 0.7, {
+        scale: 0.2,
+        opacity: 0,
+    })
+    .from(".fake-btn", 0.3, {
+        scale: 0.2,
+        opacity: 0,
+    })
+    .staggerTo(
+        ".hbd-chatbox span",
+        1.5, {
+            visibility: "visible",
+        },
+        0.05
+    )
+    .to(".fake-btn", 0.1, {
+        backgroundColor: "rgb(127, 206, 248)",
+    },
+    "+=4")
+    .to(
+        ".four",
+        0.5, {
+            scale: 0.2,
+            opacity: 0,
+            y: -150
+        },
+    "+=1")
+    .from(".idea-1", 0.7, ideaTextTrans)
+    .to(".idea-1", 0.7, ideaTextTransLeave, "+=2.5")
+    .from(".idea-2", 0.7, ideaTextTrans)
+    .to(".idea-2", 0.7, ideaTextTransLeave, "+=2.5")
+    .from(".idea-3", 0.7, ideaTextTrans)
+    .to(".idea-3 strong", 0.5, {
+        scale: 1.2,
+        x: 10,
+        backgroundColor: "rgb(21, 161, 237)",
+        color: "#fff",
+    })
+    .to(".idea-3", 0.7, ideaTextTransLeave, "+=2.5")
+    .from(".idea-4", 0.7, ideaTextTrans)
+    .to(".idea-4", 0.7, ideaTextTransLeave, "+=2.5")
+    .from(
+        ".idea-5",
+        0.7, {
+            rotationX: 15,
+            rotationZ: -10,
+            skewY: "-5deg",
+            y: 50,
+            z: 10,
+            opacity: 0,
+        },
+        "+=1.5"
+    )
+    .to(
+        ".idea-5 span",
+        0.7, {
+            rotation: 90,
+            x: 8,
+        },
+        "+=1.4"
+    )
+    .to(
+        ".idea-5",
+        0.7, {
+            scale: 0.2,
+            opacity: 0,
+        },
+        "+=2"
+    )
+   
+    .staggerFrom(
+        ".idea-6 span",
+        0.8, {
+            scale: 3,
+            opacity: 0,
+            rotation: 15,
+            ease: Expo.easeOut,
+        },
+        0.2
+    )
+    .staggerTo(
+        ".idea-6 span",
+        0.8, {
+            scale: 3,
+            opacity: 0,
+            rotation: -15,
+            ease: Expo.easeOut,
+        },
+        0.2,
+        "+=1.5"
+    )
+   // Show "After getting you I have" first
+.to(".memory-1", {
+    opacity: 1,
+    y: 20,
+    duration: 1,
+    ease: "power3.out"
+})
+.to(".memory-1 .memory-photo", {
+    opacity: 1,
+    y: 0,
+    stagger: 0.3,
+    duration: 1,
+    ease: "power3.out"
+}, "-=0.8")
+// Hold then fade out
+.to(".memory-1", {
+    opacity: 0,
+    y: -200,
+    duration: 1,
+    ease: "power2.in"
+}, "+=3")
+
+// Show "Memory we spent" second
+.to(".memory-2", {
+    opacity: 1,
+    y: 20,
+    duration: 1,
+    ease: "power3.out"
+})
+.to(".memory-2 .memory-photo", {
+    opacity: 1,
+    y: 0,
+    stagger: 0.3,
+    duration: 1,
+    ease: "power3.out"
+}, "-=0.8")
+// Hold then fade out (if you want)
+.to(".memory-2", {
+    opacity: 0,
+    y: -200,
+    duration: 1,
+    ease: "power2.in"
+}, "+=3")
+
+    
+    .staggerFromTo(
+        ".baloons img",
+        2.5, {
+            opacity: 0.9,
+            y: 1400,
+        }, {
+            opacity: 1,
+            y: -1000,
+        },
+        0.2
+    )
+    .from(
+        ".profile-picture",
+        0.5, {
+            scale: 3.5,
+            opacity: 0,
+            x: 25,
+            y: -25,
+            rotationZ: -45,
+        },
+        "-=2"
+    )
+    .from(".hat", 0.5, {
+        x: -100,
+        y: 350,
+        rotation: -180,
+        opacity: 0,
+    })
+    .staggerFrom(
+        ".wish-hbd span",
+        0.7, {
+            opacity: 0,
+            y: -50,
+            // scale: 0.3,
+            rotation: 150,
+            skewX: "30deg",
+            ease: Elastic.easeOut.config(1, 0.5),
+        },
+        0.1
+    )
+    .staggerFromTo(
+        ".wish-hbd span",
+        0.7, {
+            scale: 1.4,
+            rotationY: 150,
+        }, {
+            scale: 1,
+            rotationY: 0,
+            color: "#FFD700",
+            ease: Expo.easeOut,
+        },
+        0.1,
+        "party"
+    )
+    .from(
+        ".wish h5",
+        0.5, {
+            opacity: 0,
+            y: 10,
+            skewX: "-15deg",
+        },
+        "party"
+    )
+    .staggerTo(
+        ".eight svg",
+        1.5, {
+            visibility: "visible",
+            opacity: 0,
+            scale: 80,
+            repeat: 3,
+            repeatDelay: 1.4,
+        },
+        0.3
+    )
+    .to(".six", 0.5, {
+        opacity: 0,
+        y: 30,
+        zIndex: "-1",
+    })
+    .staggerFrom(".nine p", 1, ideaTextTrans, 1.2)
+    .to(
+        ".last-smile",
+        0.5, {
+            rotation: 90,
+        },
+        "+=1"
+    );
+    $(".view-gallery button").on("click", function () {
+        $(".box").toggleClass("animated");
+        // $('ul').css({
+        //   'max-width' : '100%'
+        // });
+      });
+      
+   
+
+
+     
+
+    // Restart Animation on click
+    const replyBtn = document.getElementById("replay");
+    replyBtn.addEventListener("click", () => {
+        tl.restart();
+    });
+}
